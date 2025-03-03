bc23b13e520af0988b10b484da631f0bc36d30b5
fusik <jakubfus.priv@gmail.com>
2025-03-03 19:39:12 +0100

    Add project files.

---
+++ Category.cs
@@ -0,0 +1,9 @@
+namespace EShopService.Models
+{
+    public class Category : BaseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
